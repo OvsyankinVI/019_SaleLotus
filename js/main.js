5cfb7dd9286f533f7a86ed4a795522471c1ae919
OvsyankinVI <ovsyankin717@gmail.com>
2023-06-08 19:48:10 +0400

    230608_Add Product

--- js/main.js
+++ js/main.js
@@ -39,29 +39,12 @@ const swiper = new Swiper('.swiper', {
 });
 
 const swipercl = new Swiper('.swiper-cl', {
-  slidesPerView: 1.4,
+  slidesPerView: 1,
    rewind: true,
+   direction: "horizontal",
    pagination: {
     el: '.swiper-pagination',
   },
-/*   spaceBetween: 24, */
-/*  breakpoints: {
-         1000: {
-          slidesPerView: 3,
-      }, 
-      850: {
-        slidesPerView: 2.5,
-    }, 
-          650: {
-        slidesPerView: 2,
-    }, 
-      450: {
-        slidesPerView: 1.5,
-    }, 
-    390: {
-      slidesPerView: 1.1,
-  }, 
-  } */
 });
 
 var swiper1 = new Swiper(".mySwiper", {
